511a698171562d13168ec9867caa9e65988103aa
Dániel Havelda <8553631dev@gmail.com>
2022-10-05 19:06:10 +0200

    Improve Bubble sort

--- src/algo/bubble-sort/bubble-sort.ts
+++ src/algo/bubble-sort/bubble-sort.ts
@@ -2,7 +2,7 @@ export default function bubbleSort(input: number[]): number[] | Error {
   if (input.length === 0) throw new Error("Length of the input should be greater than 0.");
 
   for (let i = 0; i < input.length; i++) {
-    for (let j = 0; j < input.length - 1; j++) {
+    for (let j = 0; j < input.length - 1 - i; j++) {
       if (input[j] > input[j + 1]) {
         const temp = input[j + 1];
         input[j + 1] = input[j];
