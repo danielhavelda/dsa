57a21007a9e9c701844de3d7aa3d0902b689648e
Dániel Havelda <8553631dev@gmail.com>
2022-10-05 13:16:06 +0200

    Change jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    verbose: false,
-    rootDir: "./src"
-  };
+  preset: "ts-jest",
+  testEnvironment: "node",
+  verbose: true,
+  rootDir: "./src",
+};
